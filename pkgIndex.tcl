f630d51aa31ce0c20552ad5e81398a9cecc0697f
Danilo Chang <ray2501@gmail.com>
2017-01-08 01:01:08 +0800

    Setup version to v0.3

--- pkgIndex.tcl
+++ pkgIndex.tcl
@@ -1,5 +1,5 @@
 #
 # Tcl package index file
 #
-package ifneeded snappy 0.2 \
-    [list load [file join $dir libsnappy0.2.so] snappy]
+package ifneeded snappy 0.3 \
+    [list load [file join $dir libsnappy0.3.so] snappy]
