e14dffcf72dee4d6e8a1bd781a11cb245e0f0321
Danilo Chang <ray2501@gmail.com>
2023-12-14 10:50:35 +0800

    Setup version to v0.12

--- pkgIndex.tcl
+++ pkgIndex.tcl
@@ -2,9 +2,9 @@
 # Tcl package index file, version 1.1
 #
 if {[package vsatisfies [package provide Tcl] 9.0-]} {
-    package ifneeded snappy 0.11 \
-	    [list load [file join $dir libtcl9snappy0.11.so] [string totitle snappy]]
+    package ifneeded snappy 0.12 \
+	    [list load [file join $dir libtcl9snappy0.12.so] [string totitle snappy]]
 } else {
-    package ifneeded snappy 0.11 \
-	    [list load [file join $dir libsnappy0.11.so] [string totitle snappy]]
+    package ifneeded snappy 0.12 \
+	    [list load [file join $dir libsnappy0.12.so] [string totitle snappy]]
 }
--- tcl.snappy_SUSE.spec
+++ tcl.snappy_SUSE.spec
@@ -4,7 +4,7 @@
 
 Name:          tcl.snappy
 Summary:       Tcl wrapper for snappy library
-Version:       0.11
+Version:       0.12
 Release:       0
 License:       BSD-2-Clause
 Group:         Development/Libraries/Tcl
