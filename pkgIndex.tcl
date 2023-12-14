1713af7b29ecb255906704a9625cef9003704484
Danilo Chang <ray2501@gmail.com>
2023-12-14 10:43:04 +0800

    Update tclconfig files

--- pkgIndex.tcl
+++ pkgIndex.tcl
@@ -1,5 +1,10 @@
+# -*- tcl -*-
+# Tcl package index file, version 1.1
 #
-# Tcl package index file
-#
-package ifneeded snappy 0.11 \
-    [list load [file join $dir libsnappy0.11.so] snappy]
+if {[package vsatisfies [package provide Tcl] 9.0-]} {
+    package ifneeded snappy 0.11 \
+	    [list load [file join $dir libtcl9snappy0.11.so] [string totitle snappy]]
+} else {
+    package ifneeded snappy 0.11 \
+	    [list load [file join $dir libsnappy0.11.so] [string totitle snappy]]
+}
